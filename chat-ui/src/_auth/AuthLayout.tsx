dbbc87ebffafce5017a7f9153d066c5ee3ca0a44
revathi38 <revathi.official8@gmail.com>
2023-11-10 15:42:14 +0530

    feat: sign-in UI

--- chat-ui/src/_auth/AuthLayout.tsx
+++ chat-ui/src/_auth/AuthLayout.tsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
 
 const backgroundImageStyle = {
-  backgroundImage: 'url("../public/auth_background.jpeg")',
+  backgroundImage: 'url("/auth_background.jpeg")',
   backgroundSize: "cover",
   backgroundPosition: "center center",
   backgroundRepeat: "no-repeat",
-  backgroundAttachment: "fixed", // Optional, keeps the background fixed while scrolling
-  height: "100vh", // Adjust the height based on your design
+  backgroundAttachment: "fixed",
+  height: "100vh",
 };
 
 const AuthLayout = () => {
   return (
-    <section style={backgroundImageStyle} className="flex justify-center items-center ">
-      <div className="bg-white w-1/4 p-3">
+    <section
+      style={backgroundImageStyle}
+      className="flex justify-center items-center "
+    >
+      <div className="bg-white w-full md:w-1/2 lg:w-1/3 xl:1/5 p-5 min-h-96 rounded-xl flex justify-center items-center mx-4  ">
         <Outlet />
       </div>
     </section>
--- chat-ui/src/_auth/forms/SignIn.tsx
+++ chat-ui/src/_auth/forms/SignIn.tsx
@@ -1,5 +1,77 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email format" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <p className="text-center text-3xl font-bold">Sign In</p>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter Password"
+                  type="password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignIn;
